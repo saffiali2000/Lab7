0c0ce549bb0b49b5ec72bab90f48604e73d9d409
saffiali <saffiali@cmail.carleton.ca>
2022-11-14 18:10:10 -0500

    Moved method getBillableAmount to class Site

--- src/Site.java
+++ src/Site.java
@@ -2,4 +2,7 @@ public class Site {
     protected static final double TAX_RATE = 1.13;
     public Site(){
     }
+    public double getBillableAmount(){
+        return getBasemount() + getTaxAmount();
+    }
 }
